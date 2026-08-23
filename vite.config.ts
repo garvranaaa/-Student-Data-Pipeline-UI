d49d0b2f43a135401f70c55e4d61891767bd8e19
Garv Sanjeev Rana <garvsrana@gmail.com>
2026-08-24 01:21:52 +0530

    chore: update base url for renamed repository

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/-Student-Data-Pipeline-UI/',
+  base: '/Student-Data-Pipeline-UI/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
